ef53244404c68e2c586db9f1020075aad7aa0bcc
Keith Ratchford <kdr60@zips.uakron.edu>
2022-01-25 14:29:30 -0500

    Create GameBoard Class

---
+++ MateInZero/GameBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//GameBoard.cs
+//Represent a chess board via a grid
+namespace MateInZero
+{
+    public class GameBoard
+    {
+
+    }
+}
