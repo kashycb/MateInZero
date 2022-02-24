94f87a52be3a05460c0937cecc5d0a54fd86bde2
Keith Ratchford <kdr60@zips.uakron.edu>
2022-02-24 18:43:47 -0500

    Create Move class

---
+++ MateInZero/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateInZero
+{
+    internal class Move
+    {
+        int moveValue; //How valuable the move is percieved to be by the suggesting piece
+        string startingSquare; //Where the moving piece is located
+        string endingSquare; //Where the moving piece is moving to
+        Piece actor; //The piece that wishes to move
+    }
+}
